f0ae3e044ad631d8ec167da55db120b5fa49a81a
gitcseme <shuvo_CSE23@yahoo.com>
2022-06-22 22:47:31 +0600

    cherry pickup 3d memorization

--- 5_grid_dp_variable_start_end.cpp
+++ 5_grid_dp_variable_start_end.cpp
@@ -69,6 +69,45 @@ int solve_tabulation() {
     return ans;
 }
 
+/* Recursion
+ * TC -> O(3^n * 3^n)
+ * SC -> O(n)
+ * 
+ * Memorization
+ * TC -> O(n*m*m)
+ * SC -> O(n*m*m) mem array + O(n) stack space
+*/
+
+vector<vector<vector<int>>> mem3;
+int solve_cherry(int i, int j1, int j2) {
+    if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) // Boundary case
+        return -1e8;
+    
+    if (i == n-1) {     // Base case
+        if (j1 == j2) 
+            return mat[i][j1];
+        else 
+            return mat[i][j1] + mat[i][j2];
+    }
+
+    if (mem3[i][j1][j2] != -1) return mem3[i][j1][j2];
+    
+    int maxi = -1e8;
+    for (int dj1 = -1; dj1 <= 1; ++dj1) {
+        for (int dj2 = -1; dj2 <= 1; ++dj2) {
+            int local;
+            if (j1 == j2)
+                local = mat[i][j1] + solve_cherry(i+1, j1+dj1, j2+dj2);
+            else 
+                local = mat[i][j1] + mat[i][j2] + solve_cherry(i+1, j1+dj1, j2+dj2);
+            
+            maxi = max(maxi, local);
+        }
+    }
+
+    return mem3[i][j1][j2] = maxi;
+}
+
 int main() {
     n = 4, m = 4;
     mem.resize(n, vector<int>(m, -1));
@@ -92,6 +131,18 @@ int main() {
     cout << "memorize: " << ans_m << "\n";
 
     cout << "tabulation: " << solve_tabulation() << "\n";
+
+    // cherry pickup
+    n = 3, m = 4;
+    mem3.resize(n, vector<vector<int>>(m, vector<int>(m, -1)));
+
+    mat = {
+        { 2, 3, 1, 2 },
+        { 3, 4, 2, 2 },
+        { 5, 6, 3, 5 }
+    };
+
+    cout << "\ncherry pickup: " << solve_cherry(0, 0, m-1) << "\n";
 
     return 0;
 }
