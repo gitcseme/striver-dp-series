78cec623f1126894737280108824672b49df142d
gitcseme <shuvo_CSE23@yahoo.com>
2022-06-24 12:04:46 +0600

    solved with itarative dp

--- 6_sub_sequence_dp.cpp
+++ 6_sub_sequence_dp.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 vector<int> a;
 vector<vector<int>> mem;
-int target;
 
 /* Recursion
  * TC -> O(2^n)
@@ -29,12 +28,36 @@ bool solve(int i, int target) {
     return mem[i][target] = (int)(not_taken || taken);
 }
 
+/* Tabulation
+ * TC -> O(n*target)
+ * SC -> O(n*target) mem array
+*/
+bool solve_tabulation(int n, int k) {
+    vector<vector<bool>> dp(n, vector<bool>(k+1, false));
+    for (int i = 0; i < n; ++i) dp[i][0] = true;
+    dp[0][a[0]] = true;
+
+    for (int i = 1; i < n; ++i) {
+        for (int target = 1; target <= k; ++target) {
+            int not_taken = dp[i-1][target];
+            int taken = false;
+            if (!not_taken && target >= a[i]) {
+                taken = dp[i-1][target-a[i]];
+            }
+
+            dp[i][target] = (int)(not_taken || taken);
+        }
+    }
+    return dp[n-1][k];
+}
+
 int main()
 {
     a = { 1, 3, 4, 6, 2, 1 };
-    target = 7;
+    int target = 19;
     int n = a.size();
     mem.resize(n, vector<int>(target+1, -1));
 
     cout << "memorization: " << solve(n-1, target) << "\n";
+    cout << "tabulation: " << solve_tabulation(n, target) << "\n";
 }
