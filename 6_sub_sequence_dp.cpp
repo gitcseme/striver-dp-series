a562ba3823890ad382fa3722f88b9e2f9cc2b010
gitcseme <shuvo_CSE23@yahoo.com>
2022-06-28 09:11:58 +0600

    no. of sum equal k

--- 6_sub_sequence_dp.cpp
+++ 6_sub_sequence_dp.cpp
@@ -73,15 +73,51 @@ int min_abs_diff_2subset(int n) {
     int min_diff = k + 1;
     for (int i = 0; i <= k; ++i) {
         if (dp[n-1][i])
-            min_diff = min(min_diff, abs(total - i));
+            min_diff = min(min_diff, abs(total - 2*i));
+            // S - S1 - S1
     }
 
     return min_diff;
 }
 
+int solve_count(int i, int target) {
+    if (target == 0) return 1;
+    if (i == 0) return (int)(a[0] == target);
+
+    if (mem[i][target] != -1) return mem[i][target];
+
+    int not_taken = solve_count(i-1, target);
+    int taken = 0;
+    if (a[i] <= target) {
+        taken = solve_count(i-1, target-a[i]);
+    }
+
+    return mem[i][target] = not_taken + taken;
+}
+
+int solve_count_tabulation(int n, int k) {
+    vector<vector<int>> dp(n, vector<int>(k+1, 0));
+    for (int i = 0; i < n; ++i) dp[i][0] = 1;
+    dp[0][a[0]] = 1;
+
+    for (int i = 1; i < n; ++i) {
+        for (int target = 1; target <= k; ++target) {
+            int not_taken = dp[i-1][target];
+            int taken = 0;
+            if (a[i] <= target) {
+                taken = dp[i-1][target-a[i]];
+            }
+
+            dp[i][target] = not_taken + taken;
+        }
+    }
+
+    return dp[n-1][k];
+}
+
 int main()
 {
-    a = { 3, 2, 7 };
+    a = { 3, 2, 7, 5, 1, 4 };
     int target = 9;
     int n = a.size();
     mem.resize(n, vector<int>(target+1, -1));
@@ -89,5 +125,9 @@ int main()
     cout << "memorization: " << solve(n-1, target) << "\n";
     cout << "tabulation: " << solve_tabulation(n, target) << "\n\n";
 
-    cout << "Minumum subset diff: " << min_abs_diff_2subset(n) << "\n";
+    cout << "minumum subset diff: " << min_abs_diff_2subset(n) << "\n";
+
+    fill(mem.begin(), mem.end(), vector<int>(target+1, -1));
+    cout << "freq of sum k: " << solve_count(n-1, target) << "\n";
+    cout << "freq of sum k tabulation: " << solve_count_tabulation(n, target) << "\n";
 }
