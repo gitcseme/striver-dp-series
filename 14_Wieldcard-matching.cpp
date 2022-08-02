9379c735fff37282d621e0ce121d598d636ac89b
Md. Kawsarul Alam Shuvo <shuvo_CSE23@yahoo.com>
2022-08-02 10:39:03 +0600

    created bottom-up dp for wildcard matching

--- 14_Wieldcard-matching.cpp
+++ 14_Wieldcard-matching.cpp
@@ -36,12 +36,46 @@ bool solve(int i, int j) {
     return mem[i][j] = (int)false;
 }
 
+bool solve_tabulation(int n, int m) {
+    vector<vector<int>> dp(n+1, vector<int>(m+1, false));
+    dp[0][0] = true;
+    for (int j = 1; j <= m; ++j) dp[0][j] = false;
+    for (int i = 1; i <= n; ++i) {
+        bool flag = true;
+        for (int k = 1; k <= i; ++k) {
+            if (s[k-1] != '*') {
+                flag = false;
+                break;
+            }
+        }
+        dp[i][0] = flag;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (s[i-1] == t[j-1] || s[i-1] == '?') {
+                dp[i][j] = (int)dp[i-1][j-1];
+            }
+            else if (s[i-1] == '*') {
+                bool way1 = dp[i][j-1]; // keep i in place of * and match more from j
+                bool way2 = dp[i-1][j-1]; // finished matched * and let match rest
+                dp[i][j] = (int)(way1 || way2);
+            }
+            else
+                dp[i][j] = (int)false;
+        }
+    }
+
+    return dp[n][m];
+}
+
 int main() {
     int n = s.length();
     int m = t.length();
     mem.resize(n, vector<int>(m, -1));
 
-    cout << solve(n-1, m-1) << "\n";
+    cout << "Memorization: " << solve(n-1, m-1) << "\n";
+    cout << "Tabulation: " << solve_tabulation(n, m) << "\n";
 
     return 0;
 }
