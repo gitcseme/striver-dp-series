58257a4390f988fb9dc3e4852c1328909510a54b
gitcseme <shuvo_CSE23@yahoo.com>
2022-08-06 13:04:27 +0600

    solved with tabulation

--- 15_Buy_and_Sell_stock.cpp
+++ 15_Buy_and_Sell_stock.cpp
@@ -28,10 +28,30 @@ int solve(int i, int buy) {
     return mem[i][buy] = profit;
 }
 
+int sovle_tabulation(int n) {
+    vector<vector<int>> dp(n+1, vector<int>(2, 0));
+    dp[n][0] = dp[n][1] = 0;
+
+    for (int i = n-1; i >= 0; --i) {
+        for (int buy = 0; buy <= 1; ++buy) {
+            int profit = 0;
+            if (buy) {
+                profit = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+            }
+            else {
+                profit = max(prices[i] + dp[i+1][1], dp[i+1][0]);
+            }
+            dp[i][buy] = profit;
+        }
+    }
+    return dp[0][1];
+}
+
 int main() {
     prices = { 7, 1, 5, 3, 6, 4 };
     n = prices.size();
     mem.resize(n, vector<int>(2, -1));
 
     cout << solve(0, 1) << "\n";
+    cout << sovle_tabulation(n) << "\n";
 }
