13b94b63277be70bee6cf77e6baf58b3391ce4af
gitcseme <shuvo_CSE23@yahoo.com>
2022-10-23 12:31:34 +0600

    bipartite with dfs

--- graph-series/bipartite-graph.cpp
+++ graph-series/bipartite-graph.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
+/*  
     TC -> O(N+E)
-    SC -> O(N+E) + O(N) for queue + O(N) for color array
+    (BFS) -> SC -> O(N+E) + O(N) for queue + O(N) for color array
+    (DFS) -> SC -> O(N+E) + O(N) for recursion stack + O(N) for color array
 */
 int n = 10;
 
 vector<vector<int>> g(n);
 vector<int> color(n, -1);
 
-bool checkBipartiteBfs(int i) {
+bool bfsCheck(int i) {
     queue<int> q;
     q.push(i);
     color[i] = 1;
@@ -32,6 +33,22 @@ bool checkBipartiteBfs(int i) {
     return true;
 }
 
+bool dfsCheck(int u) {
+    if (color[u] == -1) color[u] = 1;
+
+    for (int v : g[u]) {
+        if (color[v] == -1) {
+            color[v] = 1 - color[u];
+            if (dfsCheck(v) == false) 
+                return false;
+        }
+        else if (color[v] == color[u])
+            return false;
+    }
+
+    return true;
+}
+
 int main() {
     g[1] = {2};
     g[2] = {1, 3, 7};
@@ -43,16 +60,30 @@ int main() {
     g[8] = {5};
 
     // there are multiple separated graph part hence looping
+    bool bfsFlag = true;
     for (int i = 1; i <= 8; ++i) {
         if (color[i] == -1) {
-            if (checkBipartiteBfs(i) == false) {
-                cout << "Not Bipartite!!" << "\n";
-                return 0;
+            if (bfsCheck(i) == false) {
+                bfsFlag = false;
+                break;
             }
         }
     }
+    if (bfsFlag) cout << "(BFS)-> Bipartite Graph!!" << "\n";
 
-    cout << "Bipartite Graph!!" << "\n";
+    fill(color.begin(), color.end(), -1);
+
+    bool dfsFlag = true;
+    for (int i = 1; i <= 8; ++i) {
+        if (color[i] == -1) {
+            if (dfsCheck(i) == false) {
+                dfsFlag = false;
+                break;
+            }
+        }
+    }
+    if (dfsFlag) cout << "(DFS)-> Bipartite Graph!!" << "\n";
+
     return 0;
     
 }
