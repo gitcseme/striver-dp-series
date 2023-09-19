6938e0512543bc279898170dd04e600e80157132
Md. Kawsarul Alam Shuvo <shuvo_CSE23@yahoo.com>
2023-09-19 10:33:29 +0600

    Update 15_Buy_and_Sell_stock.cpp

--- 15_Buy_and_Sell_stock.cpp
+++ 15_Buy_and_Sell_stock.cpp
@@ -1,47 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minBuy = prices[0];
+        int maxProfit = 0;
+        for (int i = 0, n = prices.size(); i < n; ++i) {
+            if (minBuy > prices[i]) {
+                minBuy = prices[i];
+            }
 
-vector<int> prices;
-int n;
-vector<vector<int>> mem;
+            maxProfit = max(maxProfit, prices[i] - minBuy);
+        }
 
-/* Recursion:
- * TC -> O(2^n)
- * SC -> O(n) auxiliary stack space
- * 
- * Memorization:
- * TC -> O(n*2)
- * SC -> O(n) O(n) auxiliary stack space
-*/
-
-/* buy = 1 => can buy stock, 0 => can't
-*/
-int solve(int i, int buy) {
-    if (i == n) return 0;
-    if (mem[i][buy] != -1) return mem[i][buy];
-
-    int profit = 0;
-    if (buy) {
-        profit = max(-prices[i] + solve(i+1, 0), solve(i+1, 1));
+        return maxProfit;
     }
-    else {
-        profit = max(prices[i] + solve(i+1, 1), solve(i+1, 0));
-    }
-
-    return mem[i][buy] = profit;
-}
-
-int solve_tabulation(int n) {
-    vector<vector<int>> dp(n+1, vector<int>(2, 0));
-    
-}
-
-int main() {
-    prices = { 7,1,5,3,6,4 };
-    n = prices.size();
-    mem.resize(n, vector<int>(2, -1));
-
-    // cout << solve(0, 0) << "\n";
-    //cout << solve(0, 1) << "\n";
-    cout << solve_tabulation(n) << "\n";
-}
+};
